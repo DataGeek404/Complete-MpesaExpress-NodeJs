67459506f910ff726d5452cc505c31c777484774
DataGeek404 <techjayprivate@gmail>
2025-12-09 23:41:10 +0300

    backend

---
+++ backend/src/app/layout.js
@@ -0,0 +1,13 @@
+export const metadata = {
+    title: 'M-Pesa Gateway',
+    description: 'M-Pesa Integration Backend',
+  }
+  
+  export default function RootLayout({ children }) {
+    return (
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    )
+  }
+  
